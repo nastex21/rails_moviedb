ae4ea088a591a15d25f454a70ee4d54a6080dcb8
nastex21 <nasax2000@gmail.com>
2021-02-19 01:44:04 -0600

    Cleaned up for next part.

--- client/angular-moviedb/src/app/app.component.ts
+++ client/angular-moviedb/src/app/app.component.ts
@@ -10,15 +10,5 @@ export class AppComponent {
   title = 'angular-moviedb';
     constructor(private authToken: AngularTokenService){
 
-      this.authToken.signIn({login: 'user@example.com', password: 'monkey67'}).subscribe(
-          res => {
-            console.log('auth response:', res);
-            console.log('auth response headers: ', res.headers.toJSON()); // log the response header to show the auth token
-            console.log('auth response body:', res.json()); // log the response body to show the user 
-          },
-          err => {
-            console.error('auth error:', err);
-          }
-      );
     }
 }
