ad3b65585c54821c23c67d7c4eba1b79d09608d4
nastex21 <nasax2000@gmail.com>
2021-02-19 00:30:57 -0600

    Revert "Not getting HTTP error anymore but I am getting some other errors."
    This reverts commit 49e0247381319300726125c0f34a24c3f1d0f029.

--- client/angular-moviedb/src/app/app.module.ts
+++ client/angular-moviedb/src/app/app.module.ts
@@ -4,22 +4,17 @@ import { MaterializeModule } from 'angular2-materialize';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Angular2TokenService } from 'angular2-token';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    MaterializeModule,
-    FormsModule,
+    MaterializeModule
   ],
-  providers: [ Angular2TokenService ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
