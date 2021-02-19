ad9e58a424dc2b614004fb2e0b0954b64790346f
nastex21 <nasax2000@gmail.com>
2021-02-19 01:16:03 -0600

    Compiled with no errors.

--- client/angular-moviedb/src/app/app.component.ts
+++ client/angular-moviedb/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularTokenService } from 'angular-token';
 
 @Component({
   selector: 'lets-react-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-moviedb';
+    constructor(private authToken: AngularTokenService){
+
+      this.authToken.signIn({login: 'user@example.com', password: 'monkey67'}).subscribe(
+          res => {
+            console.log('auth response:', res);
+            console.log('auth response headers: ', res.headers.toJSON()); // log the response header to show the auth token
+            console.log('auth response body:', res.json()); // log the response body to show the user 
+          },
+          err => {
+            console.error('auth error:', err);
+          }
+      );
+    }
 }
