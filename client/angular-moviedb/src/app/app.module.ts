49e0247381319300726125c0f34a24c3f1d0f029
nastex21 <nasax2000@gmail.com>
2021-02-19 00:27:16 -0600

    Not getting HTTP error anymore but I am getting some other errors.

--- client/angular-moviedb/src/app/app.module.ts
+++ client/angular-moviedb/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { MaterializeModule } from 'angular2-materialize';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Angular2TokenService } from 'angular2-token';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    MaterializeModule
+    MaterializeModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [ Angular2TokenService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
